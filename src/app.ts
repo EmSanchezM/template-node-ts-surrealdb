cc04f2e6c81cfc1f11552df05bed3a1352db71c6
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2025-02-22 11:05:21 -0600

    feat(database): :sparkles: add database connection instance

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+
+import { getDatabaseConnection } from './database/connection';
+
+const app = async () => {
+  const db = await getDatabaseConnection();
+  // test health check
+  const healthcheck = await db.ping();
+  if(db.status === 'connected' && healthcheck) {
+    console.info('Connected to SurrealDB');
+  } else {
+    console.error('Failed to connect to SurrealDB');
+  }
+};
+
+app();
+
